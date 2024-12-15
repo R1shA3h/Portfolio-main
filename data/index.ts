c9ea86c6729ba2e8f3be77f602bf0e3cacdb3170
Rishabh <jain.rishabh260303@gmail.com>
2024-12-15 19:27:16 +0530

    Initial commit

---
+++ data/index.ts
@@ -0,0 +1,220 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Interests", link: "#workExperience" },
+  { name: "Projects", link: "#projects" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize collaboration, fostering open communication & take responsibility of outcomes",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for life-long learning.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a GenAI Storyteller",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Full Stack Web Development",
+    desc: "Ambitious full-stack developer driven by curiosity, committed to growth, and taking responsibility for impactful outcomes while giving back through meaningful digital solutions.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "AI, Machine Learning, and GenAI",
+    desc: "Enthusiastic and passionate learner, fascinated by evolving technologies and their potential to solve real-world problems. Exploring how AI agents can revolutionize the SaaS world and drive innovation across industries.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  // {
+  //   id: 3,
+  //   title: "Freelance App Dev Project",
+  //   desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+  //   className: "md:col-span-2", // change to md:col-span-2
+  //   thumbnail: "/exp3.svg",
+  // },
+  // {
+  //   id: 4,
+  //   title: "Lead Frontend Developer",
+  //   desc: "Developed and maintained user-facing features using modern frontend technologies.",
+  //   className: "md:col-span-2",
+  //   thumbnail: "/exp4.svg",
+  // },
+];
+export const projects = [
+  {
+    id: 1,
+    title: "Wind Forecast monitoring app",
+    des: "Visualize Wind Forecasts with Ease, Simplifying Forecast Data into Insightful Charts",
+    img: "/p1.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/prisma.svg", "/postgresql.svg"],
+    link: "https://github.com/R1shA3h/Wind-forcast",
+  },
+  {
+    id: 2,
+    title: "Journal App",
+    des: "Modern Journaling Made Simple, Built with Ruby on Rails in Just a Week",
+    img: "/p2.png",
+    iconLists: ["/ruby.svg", "/rails.svg", "/aws-s3.svg", "/javascript.svg"],
+    link: "https://github.com/R1shA3h/Journal-app-rails",
+  },
+  {
+    id: 3,
+    title: "TryOn-AI: Virtual Try-On App",
+    des: "Redefining Style with Cutting-Edge Virtual Try-On Experiences and reducing the cost of returns to sellers.",
+    img: "/tryon-ai.jpg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/tensorflow.svg", "/pytorch.svg"],
+    link: "https://github.com/R1shA3h/E-com-Virtual-TryOn",
+  },
+  {
+    id: 4,
+    title: "Smart Pest Identifier and classifier",
+    des: "AI-Powered Pest identification using image for Precise and Proactive Pest Management",
+    img: "/pest.png",
+    iconLists: ["/ml.png", "/python.svg", "/tensorflow.svg", "/streamlit.svg", "/pytorch.svg"],
+    link: "https://github.com/AnujBelgaonkar/Pest-Detection-and-Classification",
+  },
+];
+
+// export const testimonials = [
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+//   {
+//     quote:
+//       "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+//     name: "Michael Johnson",
+//     title: "Director of AlphaStream Technologies",
+//   },
+// ];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link:"https://github.com/R1shA3h"
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link:"https://x.com/R1sha3Ja1n"
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link:"https://www.linkedin.com/in/r1sha3h/  "
+  },
+];
